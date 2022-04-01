4e4f6b54295d81bfb85ab7dae26aca0f4c86e95f
dhimaseka <dimaseka83@gmail.com>
2022-04-01 13:08:50 +0700

    ++

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
         <v-app-bar color="surface" height="80" class="el" app  :style="{padding: $vuetify.breakpoint.mdAndUp ? '0px 100px': ''}">
-            <v-toolbar-title class="text-md-h5 font-weight-bold pointer" @click="$router.push('/')">MyPortfolio</v-toolbar-title>
+                <!-- Smartphone -->
+        <div v-show="this.$vuetify.breakpoint.xs">
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        </div>
+        <v-toolbar-title class="text-md-h5 font-weight-bold pointer" @click="$router.push('/')">MyPortfolio</v-toolbar-title>
         <v-spacer></v-spacer>
+        <!-- No Smartphone -->
         <div v-show="this.$vuetify.breakpoint.sm || this.$vuetify.breakpoint.md || this.$vuetify.breakpoint.lg || this.$vuetify.breakpoint.xl">
         <v-btn text nuxt to="/">Home</v-btn>
         <v-btn text nuxt to="/about">About</v-btn>
@@ -17,10 +22,50 @@
             <v-icon>light_mode</v-icon>
         </v-btn>
         </v-app-bar>
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+            <v-list nav dense>
+                <v-list-item-group v-model="group" >
+                    <v-list-item v-for="(nav, i) in navigation" :key="i" @click="$router.push(nav.path)" active-class="deep-purple--text text--accent-4">
+                        <v-list-item-icon>
+                            <v-icon>{{nav.icon}}</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>{{nav.text}}</v-list-item-title>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            drawer: false,
+            group: null,
+            navigation: [
+                {
+                    text: 'Home',
+                    icon: 'mdi-home',
+                    path: '/'
+                },
+                {
+                    text: 'About',
+                    icon: 'mdi-account',
+                    path: '/about'
+                },
+                {
+                    text: 'Project',
+                    icon: 'mdi-folder-open',
+                    path: '/project'
+                },
+                {
+                    text: 'Contact',
+                    icon: 'mdi-email',
+                    path: '/contact'
+                }
+            ]
+        }
+    },
     methods: {
         toggleTheme() {
             this.$vuetify.theme.dark = !this.$vuetify.theme.dark
